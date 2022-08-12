680cb878bc3d14a0e61947d650d8efe97432383d
sujith s <sujiths@sujiths-MacBook-Pro.local>
2022-08-12 13:04:35 +0530

    added default h2 console

--- src/main/java/com/photoapp/users/api/UsersControllers.java
+++ src/main/java/com/photoapp/users/api/UsersControllers.java
@@ -1,5 +1,7 @@
 package com.photoapp.users.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersControllers {
 
+    @Autowired
+    Environment environment;
     @GetMapping("/status/check")
     public String status() {
-        return "Working";
+        String port = environment.getProperty("local.server.port");
+        return "Working on port " + port;
     }
 
 }
